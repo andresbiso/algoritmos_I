aebb7a287549e04ff3510f44ee0d87000ee052c4
Andrés Biso <biso.andres@gmail.com>
2023-03-19 18:56:48 -0300

    add changes to unidad 2 actividad

--- unidad_2/unidad_2_actividad.c
+++ unidad_2/unidad_2_actividad.c
@@ -1,5 +1,26 @@
 /*Actividad: Análisis del tiempo de ejecución*/
 /*Detallar el tiempo de ejecución del siguiente código:*/
+
+/*
+void proc(int n)
+{
+    int i, k;
+    i = 1;
+    while (i <= n)
+    {
+        k = i;
+        while (k <= n)
+            k++;
+        k = 1;
+        while (k <= i)
+            k++;
+        i++;
+    }
+}
+*/
+
+/*Implementación Ejemplo*/
+
 #include <stdio.h>
 
 void proc(int n)
